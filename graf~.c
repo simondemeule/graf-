fd38b01d563dc8742edf8c08999e6e0368af3f7f
Simon Demeule <simondemeule@gmail.com>
2020-03-13 01:03:33 -0400

    Kernel compiles & runs but past / coeffs are uninitialized

--- graf~.c
+++ graf~.c
@@ -33,26 +33,32 @@
 // struct to represent the object's state
 typedef struct _graf {
     t_pxobject ob;            // the object itself (t_pxobject in MSP instead of t_object)
-    double samplerate;
-    long sampleframes;
+    double sample_rate;
+    long bin_size;
+    long time_offset;
+    long time_size;
     
     bool cl_init_attempted = false;
     bool cl_init_failed = false;
-    size_t cl_global;                      // global domain size for our calculation
-    size_t cl_local;                       // local domain size for our calculation
+    size_t cl_global;                // global domain size for our calculation
+    size_t cl_local;                 // domain size for our calculation
     cl_int cl_err;
     int cl_device_idx;
-    cl_device_id cl_device_id;             // compute device id
-    cl_context cl_context;                 // compute context
-    cl_command_queue cl_queue;          // compute command queue
-    cl_program cl_program;                 // compute program
-    cl_kernel cl_kernel;                   // compute kernel
+    cl_device_id cl_device_id;       // compute device id
+    cl_context cl_context;           // compute context
+    cl_command_queue cl_queue;       // compute command queue
+    cl_program cl_program;           // compute program
+    cl_kernel cl_kernel;             // compute kernel
+    cl_mem cl_mem_input;
+    cl_mem cl_mem_output;
+    cl_mem cl_mem_past;
+    cl_mem cl_mem_coeffs;
 } t_graf;
 
 // method prototypes
 void *graf_new(t_symbol *s, long argc, t_atom *argv);
 void graf_free(t_graf *x);
-void graf_dsp64(t_graf *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags);
+void graf_dsp64(t_graf *x, t_object *dsp64, short *count, double sample_rate, long maxvectorsize, long flags);
 
 void graf_float(t_graf *x, double f);
 void graf_int(t_graf *x, long l);
@@ -60,7 +66,7 @@ void graf_int(t_graf *x, long l);
 void graf_assist(t_graf *x, void *b, long m, long a, char *s);
 void graf_set(t_graf *x, t_symbol *s);
 
-void graf_perform64(t_graf *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long sampleframes, long flags, void *userparam);
+void graf_perform64(t_graf *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long bin_size, long flags, void *userparam);
 
 // global class pointer variable
 static t_class *graf_class = NULL;
@@ -79,100 +85,36 @@ void ext_main(void *r)
     graf_class = c;
 }
 
-// THE FFT HAS 2 CHANNELS. WHAT DO WE DO WITH THAT???? :(:(:(:(:(:(:(
-
-// buffer(bin, time) is a data structure containing previous output values + the current input value
-// bin  spans (0, bin_size - 1)
-// time spans (0, time_size - 1)
-//
-// buffer(0, 0)              • • •  buffer(bin_size - 1, 0)
-//   •                                •
-//   •                                •
-//   •                                •
-// buffer(0, time_size - 1)  • • •  buffer(bin_size - 1, time_size - 1)
-//
-// structure works with rolling buffer over time
-// buffer time_offset is given by time_offset
-// current input frame exists at buffer(0, time_offset) • • • buffer(bin_size, time_offset)
-// previous input frame exists at buffer(0, warp(time_offset - 1, time_size)) • • • buffer(N, warp(time_offset - 1, time_size))
-// where warp(i, time_size) is non-negative modulo M
-//
-// this 2D array is flattened with
-// buffer_flat(index) = buffer(warp(index, bin_size), index / bin_size)
-//
-// conversely we have
-// buffer(bin, time) = buffer_flat(bin + time * bin_size)
-//
-// which gives us the map
-//
-// buffer_flat(0)  • • •  buffer_flat(bin_size - 1), buffer_flat(bin_size)  • • •  buffer_flat(bin_size * time_size - 1)
-//
-// buffer(0, 0)    • • •  buffer(bin_size - 1, 0),   buffer(0, 1)           • • •  buffer(bin_size - 1, time_size - 1)
-//
-// the linear system is defined as
-//
-// output(bin_output) = sum(bin_input from 0 to bin_size - 1,
-//                      sum(time from 0 to time_size - 1,
-//                      buffer(bin_input, warp(time_offset - time, time_size)) * coefficient(bin_input, time, bin_output)))
-//
-// once output(bin_output) is computed for all bin_output, time_offset is set to warp(time_offset + 1, time_size), and buffer(x, warp(T - 1)) is set to o(x) for all x
-//
-// coefficient(bin_input, time, bin_output) is a data structure containing the system coefficients
-// bin_input  spans (0, bin_size - 1)
-// time       spans (0, time_size - 1)
-// bin_output spans (0, bin_size - 1)
-//
-//        coefficient(0, 0, bin_size - 1)   • • •  coefficient(bin_size - 1, 0, bin_size - 1)
-//         •                                        •
-//       •                                        •
-// coefficient(0, 0, 0)              • • •  coefficient(bin_size - 1, 0, 0)
-//    •                                        •
-//    •                                        •
-//    •                                        •
-// coefficient(0, time_size - 1, 0)  • • •  coefficient(bin_size - 1, time_size - 1, 0)
-//
-// this 3D array is flattened with
-// coefficient_flat(index) = coefficient(warp(index, bin_size), warp(index / bin_size, time_size), index / (bin_size * time_size))
-//
-// conversely we have
-// coefficient(bin_input, time, bin_output) = coefficient_flat(bin_input + time * bin_size + bin_output * bin_size * time_size)
-//
-// which gives us the map
-//
-// coefficient_flat(0)   • • •  coefficient_flat(bin_size - 1),  coefficient_flat(bin_size)  • • •  coefficient_flat(bin_size * time_size - 1),  coefficient_flat(bin_size * time_size)  • • •  coefficient_flat(bin_size * time_size * bin_size - 1)
-//
-// coefficient(0, 0, 0)  • • •  coefficient(bin_size - 1, 0, 0), coefficient(0, 1, 0)        • • •  coefficient(bin_size - 1, time_size - 1, 0), coefficient(0, 0, 1)                    • • •  coefficient(bin_size - 1, time_size - 1, bin_size - 1)
-//
-// kernels can only use single-dimensional arrays, so they are always implicitly the flattened versions of the array
-
-
 const char *kernel_source = "\n" \
-"int warp(int index, int size) {                                                        \n" \
-"   int temp = index % size;                                                            \n" \
-"   return temp < 0 ? temp + size : temp;                                               \n" \
-"}                                                                                      \n" \
-"__kernel void system(                                                                  \n" \
-"   __global double* buffer,                                                            \n" \
-"   __global double* coefficients,                                                      \n" \
-"   __global double* output,                                                            \n" \
-"   const long bin_size,                                                                \n" \
-"   const long time_size,                                                               \n" \
-"   const long time_offset)                                                             \n" \
-"{                                                                                      \n" \
-"   int bin_output = get_global_id(0);                                                  \n" \
-"   if(bin_output < bin_size) {                                                         \n" \
-"      double accum = 0;                                                                \n" \
-"      for(int time = 0; time < time_size; time++) {                                    \n" \
-"         int time_rolling = warp(time_offset - time, time_size);                       \n" \
-"         for(int bin_input = 0; bin_input < bin_size; bin_input++) {                   \n" \
-"            int n = bin_input + time_rolling * bin_size;                               \n" \
-"            int m = bin_input + time * bin_size + bin_output * bin_size * time_size;   \n" \
-"            accum += buffer[n] * coefficients[m];                                      \n" \
-"         }                                                                             \n" \
-"      }                                                                                \n" \
-"      output[bin_output] = accum;                                                      \n" \
-"   }                                                                                   \n" \
-"}                                                                                      \n" \
+"int warp(int index, int size) {                                                            \n" \
+"   int temp = index % size;                                                                \n" \
+"   return temp < 0 ? temp + size : temp;                                                   \n" \
+"}                                                                                          \n" \
+"__kernel void system(                                                                      \n" \
+"   __global double* input,                                                                 \n" \
+"   __global double* output,                                                                \n" \
+"   __global double* past,                                                                  \n" \
+"   __global double* coeffs,                                                                \n" \
+"   const long bin_size,                                                                    \n" \
+"   const long time_size,                                                                   \n" \
+"   const long time_offset)                                                                 \n" \
+"{                                                                                          \n" \
+"   int bin_output = get_global_id(0);                                                      \n" \
+"   if(bin_output < bin_size) {                                                             \n" \
+"      double accum = 0;                                                                    \n" \
+"      for(int bin_input = 0; bin_input < bin_size; bin_input++) {                          \n" \
+"         int i = bin_input + bin_output * bin_size * time_size;                            \n" \
+"         accum += input[bin_input] * coeffs[i];                                            \n" \
+"         for(int time = 0; time < time_size - 1; time++) {                                 \n" \
+"            int time_rolling = warp(time_offset - time, time_size - 1);                    \n" \
+"            int n = bin_input + time_rolling * bin_size;                                   \n" \
+"            int m = bin_input + (time + 1) * bin_size + bin_output * bin_size * time_size; \n" \
+"            accum += past[n] * coeffs[m];                                                  \n" \
+"         }                                                                                 \n" \
+"      }                                                                                    \n" \
+"      output[bin_output] = accum;                                                          \n" \
+"   }                                                                                       \n" \
+"}                                                                                          \n" \
 "\n";
 
 void graf_cl_init(t_graf *x) {
@@ -210,7 +152,7 @@ void graf_cl_init(t_graf *x) {
 #define DEVICE_UHD630 (1)
 #define DEVICE_I9 (0)
     
-    x->cl_device_id = device_ids[DEVICE_UHD630];
+    x->cl_device_id = device_ids[DEVICE_VEGA20];
     
     // Vega only has an edge when dimensionality goes above ~2^11
     
@@ -296,11 +238,66 @@ void graf_cl_init(t_graf *x) {
     x->cl_init_failed = false;
 }
 
+void graf_cl_init_memory(t_graf *x) {
+    x->cl_init_failed = true;
+    
+    // Setup memory
+    //
+    x->cl_mem_input = clCreateBuffer(x->cl_context, CL_MEM_READ_ONLY, sizeof(double) * x->bin_size, NULL, &(x->cl_err));
+    if (x->cl_err != CL_SUCCESS)
+    {
+        post("Error: Failed to allocate input memory! %d\n", x->cl_err);
+        return;
+    }
+    x->cl_mem_output = clCreateBuffer(x->cl_context, CL_MEM_WRITE_ONLY, sizeof(double) * x->bin_size, NULL, &(x->cl_err));
+    if (x->cl_err != CL_SUCCESS)
+    {
+        post("Error: Failed to allocate output memory! %d\n", x->cl_err);
+        return;
+    }
+    x->cl_mem_past = clCreateBuffer(x->cl_context, CL_MEM_READ_ONLY, sizeof(double) * x->bin_size * (x->time_size - 1), NULL, &(x->cl_err));
+    if (x->cl_err != CL_SUCCESS)
+    {
+        post("Error: Failed to allocate past memory! %d\n", x->cl_err);
+        return;
+    }
+    x->cl_mem_coeffs = clCreateBuffer(x->cl_context, CL_MEM_READ_ONLY, sizeof(double) * x->bin_size * x->time_size, NULL, &(x->cl_err));
+    if (x->cl_err != CL_SUCCESS)
+    {
+        post("Error: Failed to allocate coefficient memory! %d\n", x->cl_err);
+        return;
+    }
+    
+    // Set the arguments to our compute kernel
+    //
+    x->cl_err  = 0;
+    x->cl_err  = clSetKernelArg(x->cl_kernel, 0, sizeof(cl_mem), &(x->cl_mem_input));
+    x->cl_err |= clSetKernelArg(x->cl_kernel, 1, sizeof(cl_mem), &(x->cl_mem_output));
+    x->cl_err |= clSetKernelArg(x->cl_kernel, 2, sizeof(cl_mem), &(x->cl_mem_past));
+    x->cl_err |= clSetKernelArg(x->cl_kernel, 3, sizeof(cl_mem), &(x->cl_mem_coeffs));
+    x->cl_err |= clSetKernelArg(x->cl_kernel, 4, sizeof(long), &(x->bin_size));
+    x->cl_err |= clSetKernelArg(x->cl_kernel, 5, sizeof(long), &(x->time_size));
+    if (x->cl_err != CL_SUCCESS)
+    {
+        post("Error: Failed to set kernel arguments! %d\n", x->cl_err);
+        return;
+    }
+    
+    x->cl_init_failed = false;
+}
+
 void graf_cl_reset(t_graf *x) {
     clReleaseProgram(x->cl_program);
     clReleaseKernel(x->cl_kernel);
     clReleaseCommandQueue(x->cl_queue);
     clReleaseContext(x->cl_context);
+}
+
+void graf_cl_reset_memory(t_graf *x) {
+    clReleaseMemObject(x->cl_mem_input);
+    clReleaseMemObject(x->cl_mem_output);
+    clReleaseMemObject(x->cl_mem_past);
+    clReleaseMemObject(x->cl_mem_coeffs);
 }
 
 void *graf_new(t_symbol *s, long argc, t_atom *argv)
@@ -312,6 +309,8 @@ void *graf_new(t_symbol *s, long argc, t_atom *argv)
         outlet_new(x, "signal");
         // disable in-place optimisation so that the input and output buffers are distinct
         x->ob.z_misc |= Z_NO_INPLACE;
+        x->time_size = 4;
+        x->time_offset = 0;
     }
     return (x);
 }
@@ -323,11 +322,11 @@ void graf_free(t_graf *x)
 }
 
 // registers a function for the signal chain in Max
-void graf_dsp64(t_graf *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags)
+void graf_dsp64(t_graf *x, t_object *dsp64, short *count, double sample_rate, long max_vector_size, long flags)
 {
-    post("my sample rate is: %f", samplerate);
+    post("my sample rate is: %f", sample_rate);
     
-    x->samplerate = samplerate;
+    x->sample_rate = sample_rate;
     object_method(dsp64, gensym("dsp_add64"), x, graf_perform64, 0, NULL);
 }
 
@@ -360,36 +359,44 @@ void graf_assist(t_graf *x, void *b, long m, long a, char *s)
 }
 
 // this is the 64-bit perform method audio vectors
-void graf_perform64(t_graf *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long sampleframes, long flags, void *userparam)
+void graf_perform64(t_graf *x, t_object *dsp64, double **ins, long ins_size, double **outs, long outs_size, long bin_size, long flags, void *user_param)
 {
     t_double *in = ins[0];
     t_double *out = outs[0];
     
-    // possible edge case: change of sampleframes? is the graph rebuilt then?
-    
     if(!x->cl_init_attempted) {
-        graf_cl_init(x);
+        // init cl environment and memory when first buffer needs to be computed
         x->cl_init_attempted = true;
+        x->bin_size = bin_size;
+        graf_cl_init(x);
+        if(!x->cl_init_failed) {
+            graf_cl_init_memory(x);
+        }
+    }
+    if(!x->cl_init_failed && x->bin_size != bin_size) {
+        // reinit cl memory when buffer size changes
+        x->bin_size = bin_size;
+        graf_cl_reset_memory(x);
+        graf_cl_init_memory(x);
     }
     if(!x->cl_init_failed)
     {
-        cl_mem cl_mem_input = clCreateBuffer(x->cl_context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, sizeof(double) * sampleframes, in, &(x->cl_err));
+        // compute single buffer
+        
+        struct timeval  tv1, tv2;
+        gettimeofday(&tv1, NULL);
+        
+        // Write input buffer to compute memory
+        //
+        x->cl_err = clEnqueueWriteBuffer(x->cl_queue, x->cl_mem_input, CL_TRUE, 0, sizeof(double) * x->bin_size, in, 0, NULL, NULL);
         if (x->cl_err != CL_SUCCESS)
         {
-            post("Error: Failed to attach input memory! %d\n", x->cl_err);
-        }
-        cl_mem cl_mem_output = clCreateBuffer(x->cl_context, CL_MEM_WRITE_ONLY, sizeof(double) * sampleframes, NULL, &(x->cl_err));
-        if (x->cl_err != CL_SUCCESS)
-        {
-            post("Error: Failed to attach output memory! %d\n", x->cl_err);
+            post("Error: Failed to write input array! %d\n", x->cl_err);
         }
         
         // Set the arguments to our compute kernel
         //
-        x->cl_err  = 0;
-        x->cl_err  = clSetKernelArg(x->cl_kernel, 0, sizeof(cl_mem), &cl_mem_input);
-        x->cl_err |= clSetKernelArg(x->cl_kernel, 1, sizeof(cl_mem), &cl_mem_output);
-        x->cl_err |= clSetKernelArg(x->cl_kernel, 2, sizeof(long), &sampleframes);
+        x->cl_err |= clSetKernelArg(x->cl_kernel, 6, sizeof(long), &(x->time_offset));
         if (x->cl_err != CL_SUCCESS)
         {
             post("Error: Failed to set kernel arguments! %d\n", x->cl_err);
@@ -398,7 +405,7 @@ void graf_perform64(t_graf *x, t_object *dsp64, double **ins, long numins, doubl
         // Execute the kernel over the entire range of our 1d input data set
         // using the maximum number of work group items for this device
         //
-        x->cl_global = sampleframes;
+        x->cl_global = bin_size;
         x->cl_err = clEnqueueNDRangeKernel(x->cl_queue, x->cl_kernel, 1, NULL, &(x->cl_global), &(x->cl_local), 0, NULL, NULL);
         if (x->cl_err)
         {
@@ -411,21 +418,14 @@ void graf_perform64(t_graf *x, t_object *dsp64, double **ins, long numins, doubl
         
         // Read back the results from the device to verify the output
         //
-        x->cl_err = clEnqueueReadBuffer(x->cl_queue, cl_mem_output, CL_TRUE, 0, sizeof(double) * sampleframes, out, 0, NULL, NULL );
+        x->cl_err = clEnqueueReadBuffer(x->cl_queue, x->cl_mem_output, CL_TRUE, 0, sizeof(double) * bin_size, out, 0, NULL, NULL );
         if (x->cl_err != CL_SUCCESS)
         {
             post("Error: Failed to read output array! %d\n", x->cl_err);
         }
         
-        struct timeval  tv1, tv2;
-        gettimeofday(&tv1, NULL);
-        
-        clReleaseMemObject(cl_mem_input);
-        clReleaseMemObject(cl_mem_output);
-        
         gettimeofday(&tv2, NULL);
-        
-        post("Memory release time: %f seconds\n",
+        post("Execution time: %f seconds\n",
              (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
              (double) (tv2.tv_sec - tv1.tv_sec));
     }
